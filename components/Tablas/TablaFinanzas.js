df68ba23f234e480a972e0db946e9a80b1e5376a
Julian <julian.querol.p@gmail.com>
2020-08-12 14:06:08 +0200

    mejoras tabla

--- components/Tablas/TablaFinanzas.js
+++ components/Tablas/TablaFinanzas.js
@@ -8,33 +8,8 @@ import TableContainer from "@material-ui/core/TableContainer";
 import TableHead from "@material-ui/core/TableHead";
 import TablePagination from "@material-ui/core/TablePagination";
 import TableRow from "@material-ui/core/TableRow";
-import blue from "@material-ui/core/colors/blue";
-
-const columns = [
-  { id: "name", label: "Name", minWidth: 170 },
-  { id: "code", label: "ISO\u00a0Code", minWidth: 100 },
-  {
-    id: "population",
-    label: "Population",
-    minWidth: 170,
-    align: "right",
-    format: (value) => value.toLocaleString("en-US"),
-  },
-  {
-    id: "size",
-    label: "Size\u00a0(km\u00b2)",
-    minWidth: 170,
-    align: "right",
-    format: (value) => value.toLocaleString("en-US"),
-  },
-  {
-    id: "density",
-    label: "Density",
-    minWidth: 170,
-    align: "right",
-    format: (value) => value.toFixed(2),
-  },
-];
+import TableSortLabel from "@material-ui/core/TableSortLabel";
+import TableFooter from "@material-ui/core/TableFooter";
 
 const createData = (id, concepto, cantidad, fecha) => {
   return { id, concepto, cantidad, fecha };
@@ -70,61 +45,70 @@ const TablaFinanzas = ({ columnas, datos }) => {
     setPage(0);
   };
 
-  return (
-    <Paper className={classes.root}>
-      <TableContainer className={classes.container}>
-        <Table stickyHeader aria-label="sticky table">
-          <TableHead className={classes.cabecera}>
-            <TableRow className={classes.cabecera}>
-              {columnas.map((column) => (
-                <TableCell
-                  className={classes.cabecera}
-                  key={column.id}
-                  align={column.align}
-                  style={{ minWidth: column.minWidth }}
-                >
-                  {column.label}
-                </TableCell>
-              ))}
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {rows
-              .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-              .map((row) => {
-                return (
-                  <TableRow hover role="checkbox" tabIndex={-1} key={row.id}>
-                    {columnas.map((column) => {
-                      const value = row[column.id];
-                      let valor;
-                      if (column.format) {
-                        valor = column.format(value);
-                      } else {
-                        valor = value;
-                      }
-                      return (
-                        <TableCell key={column.id} align={column.align}>
-                          {valor}
-                        </TableCell>
-                      );
-                    })}
-                  </TableRow>
-                );
-              })}
-          </TableBody>
-        </Table>
-      </TableContainer>
-      <TablePagination
-        rowsPerPageOptions={[10, 25, 100]}
-        component="div"
-        count={rows.length}
-        rowsPerPage={rowsPerPage}
-        page={page}
-        onChangePage={handleChangePage}
-        onChangeRowsPerPage={handleChangeRowsPerPage}
-      />
-    </Paper>
-  );
+  if (rows && rows.length > 0) {
+    return (
+      <Paper className={classes.root}>
+        <TableContainer className={classes.container}>
+          <Table stickyHeader aria-label="sticky table">
+            <TableHead>
+              <TableRow>
+                {columnas.map((column) => (
+                  <TableCell
+                    className={classes.cabecera}
+                    key={column.id}
+                    align={column.align}
+                    style={{ minWidth: column.minWidth }}
+                  >
+                    {column.label}
+                  </TableCell>
+                ))}
+              </TableRow>
+            </TableHead>
+
+            <TableBody>
+              {rows
+                .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                .map((row) => {
+                  return (
+                    <TableRow hover role="checkbox" tabIndex={-1} key={row.id}>
+                      {columnas.map((column) => {
+                        const value = row[column.id];
+                        let valor;
+                        if (column.format) {
+                          valor = column.format(value);
+                        } else {
+                          valor = value;
+                        }
+                        return (
+                          <TableCell key={column.id} align={column.align}>
+                            {valor}
+                          </TableCell>
+                        );
+                      })}
+                    </TableRow>
+                  );
+                })}
+            </TableBody>
+          </Table>
+        </TableContainer>
+        <TablePagination
+          rowsPerPageOptions={[10, 25, 100]}
+          component="div"
+          count={rows.length}
+          rowsPerPage={rowsPerPage}
+          page={page}
+          onChangePage={handleChangePage}
+          onChangeRowsPerPage={handleChangeRowsPerPage}
+        />
+      </Paper>
+    );
+  } else {
+    return (
+      <div className="w-full bg-white rounded justify-center">
+        <p className="py-4 text-center text-xl">Sin contenido</p>
+      </div>
+    );
+  }
 };
 
 export default TablaFinanzas;
